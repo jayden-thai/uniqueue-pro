147396c6f946d32bab8f3923eaadd76ffae4004e
jayden-thai <140544489+jayden-thai@users.noreply.github.com>
2026-02-04 14:29:36 -0800

    refactor(queue): lazy load auth and queue components

--- queue-frontend/src/app/app.routes.ts
+++ queue-frontend/src/app/app.routes.ts
@@ -5,12 +5,22 @@ import { Register } from './components/register/register';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'login', component: Login },
     { 
-        path: 'queue', 
-        component: Queue,
-        canActivate: [authGuard] 
+        path: '', 
+        redirectTo: '/login', 
+        pathMatch: 'full' 
     },
-    { path: 'register', component: Register}
+    { 
+        path: 'login', 
+        loadComponent: () => import('./components/login/login').then(m => m.Login) 
+    },
+    { 
+        path: 'register',
+        loadComponent: () => import('./components/register/register').then(m => m.Register)
+    },
+    { 
+        path: 'queues/:id', 
+        loadComponent: () => import('./components/queue/queue').then(m => m.Queue),
+        canActivate: [authGuard] 
+    }
 ];
